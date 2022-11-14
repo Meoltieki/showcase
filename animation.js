b8a714562609847fcda58b4d9b9fa6d4ab197e0d
Meoltieki <melanieoltieki@gmail.com>
2022-11-14 10:44:01 +0100

    Initial commit

---
+++ animation.js
@@ -0,0 +1,20 @@
+const floppy = document.querySelector('.floppy__inner');
+
+floppy.addEventListener('click', function () {
+    floppy.classList.toggle('is-flipped');
+});
+
+
+const compaq = document.querySelector('.compaq__inner');
+
+compaq.addEventListener('click', function () {
+    compaq.classList.toggle('is-flipped');
+});
+
+const sony = document.querySelector('.sony__inner');
+
+sony.addEventListener('click', function () {
+    sony.classList.toggle('is-flipped');
+});
+
+
